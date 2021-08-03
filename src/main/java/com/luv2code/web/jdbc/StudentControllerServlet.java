146e693719d8f61c08e1159e1cb776f7aa640301
tbang-350 <59138492+tbang-350@users.noreply.github.com>
2021-08-03 12:50:59 +0300

    complete and working sample crud project

---
+++ src/main/java/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -0,0 +1,176 @@
+package com.luv2code.web.jdbc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+
+@WebServlet("/StudentControllerServlet")
+public class StudentControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private StudentDbUtil studentDbUtil;
+	
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+			
+		}catch(Exception exc){
+			throw new ServletException(exc);
+		}
+	}
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		try {
+			
+			String theCommand = request.getParameter("command");
+			
+			if(theCommand == null) {
+				theCommand = "LIST";
+			}
+			
+			switch(theCommand) {
+			case "LIST":
+				listStudents(request,response);
+				break;
+				
+			case "ADD":
+				addStudents(request,response);
+				break;
+				
+			case "LOAD":
+				loadStudent(request,response);
+				break;
+				
+			case "UPDATE":
+				updateStudent(request,response);
+				
+			case "DELETE":
+				deleteStudent(request,response);
+				
+			default:
+				listStudents(request,response);
+				//break;
+			}
+			
+			
+			//list
+			listStudents(request,response);
+			
+		} catch (Exception exc) {
+			
+			throw new ServletException(exc);
+		}
+		
+	}
+
+
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		
+		String theStudentId = request.getParameter("studentId");
+		
+		studentDbUtil.deletStudent(theStudentId);
+		
+		listStudents(request,response);
+		
+	}
+
+
+	private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		
+		int id = Integer.parseInt(request.getParameter("studentId"));
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		 
+		Student theStudent = new Student(id,firstName,lastName,email);
+		
+		studentDbUtil.updateStudent(theStudent);
+		
+		listStudents(request,response);
+		
+		
+	}
+
+
+	private void loadStudent(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		
+		String theStudentId = request.getParameter("studentId");
+		
+		Student theStudent = studentDbUtil.getStudent(theStudentId);
+		
+		request.setAttribute("THE_STUDENT", theStudent);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/update-student-form.jsp");
+		
+		dispatcher.forward(request, response);
+		
+		
+	}
+
+
+	private void addStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		
+		Student theStudent = new Student(firstName,lastName,email);  
+		
+		studentDbUtil.addStudent(theStudent);
+		
+		listStudents(request,response);
+		
+	}
+
+
+	private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		List<Student> students = studentDbUtil.getStudents();
+		
+		request.setAttribute("STUDENT_LIST", students);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+		
+		dispatcher.forward(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
